358ab5db7e168a02ca8a02758d3a3fcfc9091de2
cr0wst <steve@smcrow.net>
2017-07-26 12:51:08 -0400

    Added meta tag to enable responsive design for mobile.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,6 +3,7 @@
         <title>Lease Tracker - @yield('pageName')</title>
         <meta charset="utf-8">
         <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
         <link rel="stylesheet" href="/css/app.css">
         {{-- Should probably include this using webpack --}}
